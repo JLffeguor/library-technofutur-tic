d2668ae06725ead7e3cf853286a2d3d1e1008e3e
jerronimo31 <jerronimo31@c9583945-b8c0-7566-8871-acc4c573170b>
2011-04-05 10:37:36 +0000

    dao service - jérôme

--- LibraryTechnofuturTIC/src/service/UsersService.java
+++ LibraryTechnofuturTIC/src/service/UsersService.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import dao.UsersDao;
+
 @Service
 @Transactional
 public class UsersService {
