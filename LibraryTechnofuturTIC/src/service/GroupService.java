fdd1a2da5272932b45487bf9d22047cbd648805b
jerronimo31 <jerronimo31@c9583945-b8c0-7566-8871-acc4c573170b>
2011-04-05 09:44:15 +0000

    Add service - Jérôme

---
+++ LibraryTechnofuturTIC/src/service/GroupService.java
@@ -0,0 +1,13 @@
+package service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class GroupService {
+	@Autowired GroupDao groupDao;
+	
+	
+	
+}
---
+++ LibraryTechnofuturTIC/src/service/UsersService.java
@@ -0,0 +1,13 @@
+package service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class UsersService {
+	@Autowired
+	private UsersDao dao;
+
+}
