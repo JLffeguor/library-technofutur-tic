993fb47632b4271c60ff9307154656b17f8504cc
jerronimo31 <jerronimo31@c9583945-b8c0-7566-8871-acc4c573170b>
2011-04-05 10:37:36 +0000

    dao service - jérôme

--- trunk/LibraryTechnofuturTIC/src/service/GroupService.java
+++ trunk/LibraryTechnofuturTIC/src/service/GroupService.java
@@ -1,13 +1,22 @@
 package service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import dao.GroupDao;
+import domain.Group;
 
 @Service
 @Transactional
 public class GroupService {
 	@Autowired GroupDao groupDao;
 	
+	public List<Group> getGroups(){
+		return groupDao.getGroups();
+	}
 	
 	
 }
