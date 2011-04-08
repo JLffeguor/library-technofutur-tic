5ffb4d4b14ee01b9e53cd95d869637e519cb7a33
jerronimo31 <jerronimo31@c9583945-b8c0-7566-8871-acc4c573170b>
2011-04-08 13:14:43 +0000

    add jxel library - jérôme

---
+++ LibraryTechnofuturTIC/src/library/jxel/xlsGenerator/XlsGenerator.java
@@ -0,0 +1,64 @@
+package library.jxel.xlsGenerator;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import jxl.Workbook;
+import jxl.format.Alignment;
+import jxl.write.Label;
+import jxl.write.WritableCell;
+import jxl.write.WritableCellFormat;
+import jxl.write.WritableFont;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
+import library.dao.BookDao;
+import library.domain.Book;
+
+import org.springframework.stereotype.Service;
+@Service
+public class XlsGenerator {
+
+	public void exportAllBooks(BookDao bookDao) {
+		List<Book> books = new ArrayList<Book>();
+		books = bookDao.getBooks();
+		try {
+			WritableWorkbook workbook = Workbook.createWorkbook(new File("AllBook.xls"));
+			WritableSheet sheet = workbook.createSheet("Feuille 1", 0);
+			//set the width of the row
+			sheet.setColumnView(0, 80);
+			WritableFont arial14font = new WritableFont(WritableFont.ARIAL, 14);
+			WritableFont columnfont = new WritableFont(WritableFont.ARIAL, 20, WritableFont.BOLD);
+			
+			WritableCellFormat arial14format = new WritableCellFormat(arial14font);
+			WritableCellFormat columnformat = new WritableCellFormat(columnfont);
+			
+			arial14format.setWrap(true);
+			
+			Label columntitle = new Label (0,0,"Title",columnformat);
+			Label columnAuthor = new Label (1,0,"Author",columnformat);
+			sheet.addCell(columntitle);
+			sheet.addCell(columnAuthor);
+			
+			int i = 1;
+			for(Book b : books){
+				Label titleLabel = new Label(0,i,b.getTitle(),arial14format);
+				sheet.addCell(titleLabel);
+				Label authorLabel = new Label(1,i,b.getAuthor(),arial14format);
+				sheet.addCell(authorLabel);
+				Label isbnCell = new Label(2,i,b.getIsbn(),arial14format);
+				System.out.println( i + " : " + b.getTitle());
+				i++;
+			}
+			workbook.write();
+			workbook.close();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} catch (WriteException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+}
